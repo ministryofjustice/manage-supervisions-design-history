4c9101a4e7dd3ea3aaa5edc5c02685ad8236ea7d
Paul Hayes <fofr@users.noreply.github.com>
2021-12-22 15:03:16 +0000

    Redirect Heroku to cloud

--- app/routes.js
+++ app/routes.js
@@ -1,5 +1,10 @@
 const express = require('express')
 const router = express.Router()
+const newDomain = 'https://manage-supervisions-design-history.apps.live.cloud-platform.service.justice.gov.uk'
+
+router.use(function (req, res, next) {
+  return res.redirect(301, newDomain + req.url)
+})
 
 // Add your routes here - above the module.exports line
 
